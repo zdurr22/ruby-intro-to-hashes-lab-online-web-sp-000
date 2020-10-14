eb023fe9f2853146f079a3186cc1bde426d93883
Zachary Durr <zdurr22@gmail.com>
2020-10-14 00:27:18 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -18,7 +18,7 @@ def monopoly_with_second_tier
 end
 
 def monopoly_with_third_tier
-	monopoly = {railroads: {pieces: 4, names: {}, rent_in_dollars: {}},}
+	monopoly = {railroads: {pieces: 4, names: {}, rent_in_dollars: {one_piece_owned: 25}},}
 end
 
 def monopoly_with_fourth_tier
