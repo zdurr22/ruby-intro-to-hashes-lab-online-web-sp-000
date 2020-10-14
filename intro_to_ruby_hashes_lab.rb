99a3e9454272cbbf0eff7484f4c97467b4d1b712
Zachary Durr <zdurr22@gmail.com>
2020-10-14 00:39:55 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -27,16 +27,20 @@ def monopoly_with_fourth_tier
 		{
 			pieces: 4,
 			names: {
-				reading_railroad: {
+				:reading_railroad=>
+				{
 					mortgage_value: "$100"
 					},
-				pennsylvania_railroad: {
+				:pennsylvania_railroad=>
+				{
 					mortgage_value: "$200"
 					},
-				b_and_o_railroad: {
+				:b_and_o_railroad=> 
+				{
 					mortgage_value: "$400"
 					},
-				shortline: {
+				:shortline=>
+				{
 					mortgage_value: "$800"
 					}
 				},
